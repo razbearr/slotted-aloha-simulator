d28c28d0f61f2c0f63cfffe513cfcf2b51094d8e
Katyayni-Aarya <katyayni.aarya.rajput@gmail.com>
2025-10-31 10:01:57 +0530

    Logo

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Page configuration
 st.set_page_config(
     page_title="Network Protocol Simulator",
-    page_icon="",
+    page_icon="assets/logo.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
